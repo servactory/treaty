eb1c1e554268c609a9a668e964265d44efe0236b
Anton Sokolov <profox.rus@gmail.com>
2025-11-06 23:02:52 +0700

    Remove outdated problem comments in `ApplicationTreaty`
    - Deleted unnecessary comments describing unresolved design questions in `ApplicationTreaty`.
    - Improved code readability by cleaning up non-functional and obsolete notes.
    - No functional changes introduced.

--- spec/sandbox/app/treaties/application_treaty.rb
+++ spec/sandbox/app/treaties/application_treaty.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationTreaty < Treaty::Base
-  # Problems:
-  # - How to implement work with path and query?
-  # - How to implement filtering from query for IndexTreaty?
-  # - How to share a dataset between treaties (controller actions)?
 end
